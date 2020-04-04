1950210266d04f393c01cf0c469509e8d022f856
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-04-04 21:10:13 +0000

    *Fix a typo.

--- libs/operations/accountupdateoperation.go
+++ libs/operations/accountupdateoperation.go
@@ -18,4 +18,48 @@ func init() {
 type AccountUpdateOperation struct {
 	types.OperationFee
 	Account    types.AccountID               `json:"account"`
-	Active     *types.Authority           
+	Active     *types.Authority              `json:"active,omitempty"`
+	Extensions types.AccountUpdateExtensions `json:"extensions"`
+	NewOptions *types.AccountOptions         `json:"new_options,omitempty"`
+	Owner      *types.Authority              `json:"owner,omitempty"`
+}
+
+func (p AccountUpdateOperation) Type() types.OperationType {
+	return types.OperationTypeAccountUpdate
+}
+
+func (p AccountUpdateOperation) MarshalFeeScheduleParams(params types.M, enc *util.TypeEncoder) error {
+	if fee, ok := params["fee"]; ok {
+		if err := enc.Encode(types.UInt64(fee.(float64))); err != nil {
+			return errors.Annotate(err, "encode Fee")
+		}
+	}
+
+	if ppk, ok := params["price_per_kbyte"]; ok {
+		if err := enc.Encode(types.UInt32(ppk.(float64))); err != nil {
+			return errors.Annotate(err, "encode PricePerKByte")
+		}
+	}
+
+	return nil
+}
+
+func (p AccountUpdateOperation) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.Encode(int8(p.Type())); err != nil {
+		return errors.Annotate(err, "encode OperationType")
+	}
+
+	if err := enc.Encode(p.Fee); err != nil {
+		return errors.Annotate(err, "encode Fee")
+	}
+
+	if err := enc.Encode(p.Account); err != nil {
+		return errors.Annotate(err, "encode Account")
+	}
+
+	if err := enc.Encode(p.Owner != nil); err != nil {
+		return errors.Annotate(err, "encode have Owner")
+	}
+
+	if err := enc.Encode(p.Owner); err != nil {
+		re
