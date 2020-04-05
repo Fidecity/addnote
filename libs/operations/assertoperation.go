f959bea509eb67cd5f8bc54b4c3653807b3c6a54
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-04-05 08:42:12 +0000

    Delete commented out code

--- libs/operations/assertoperation.go
+++ libs/operations/assertoperation.go
@@ -35,4 +35,37 @@ func init() {
 
 // struct block_id_predicate
 // {
-//    block_id_ty
+//    block_id_type id;
+//    bool validate()const{ return true; }
+// };
+
+// typedef static_variant<
+//    account_name_eq_lit_predicate,
+//    asset_symbol_eq_lit_predicate,
+//    block_id_predicate
+// > predicate;
+
+type AssertOperation struct {
+	types.OperationFee
+	// asset                      fee;
+	// account_id_type            fee_paying_account;
+	// vector<predicate>          predicates;
+	// flat_set<account_id_type>  required_auths;
+	// extensions_type extensions;
+}
+
+func (p AssertOperation) Type() types.OperationType {
+	return types.OperationTypeAssert
+}
+
+func (p AssertOperation) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.Encode(int8(p.Type())); err != nil {
+		return errors.Annotate(err, "encode OperationType")
+	}
+	if err := enc.Encode(p.Fee); err != nil {
+		return errors.Annotate(err, "encode Fee")
+	}
+	//(fee)(fee_paying_account)(predicates)(required_auths)(extensions)
+	logging.Warnf("%s is not implemented", p.Type().OperationName())
+	return nil
+}
---
+++ libs/operations/assetclaimfeesoperation.go
@@ -0,0 +1,6 @@
+package operations
+
+//go:generate ffjson $GOFILE
+
+import (
+	"github.com/Assetsadapter/whitecoin-adapter/lib
