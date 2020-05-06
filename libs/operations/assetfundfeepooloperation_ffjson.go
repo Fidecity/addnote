3ac9d9227fd92ed0ca4fedf24c8e5c28f462160f
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-05-06 20:28:26 +0000

    ld/

--- libs/operations/assetfundfeepooloperation_ffjson.go
+++ libs/operations/assetfundfeepooloperation_ffjson.go
@@ -111,4 +111,50 @@ var ffjKeyAssetFundFeePoolOperationFromAccount = []byte("from_account")
 
 var ffjKeyAssetFundFeePoolOperationFee = []byte("fee")
 
-// UnmarshalJSON umarshall json - temp
+// UnmarshalJSON umarshall json - template of ffjson
+func (j *AssetFundFeePoolOperation) UnmarshalJSON(input []byte) error {
+	fs := fflib.NewFFLexer(input)
+	return j.UnmarshalJSONFFLexer(fs, fflib.FFParse_map_start)
+}
+
+// UnmarshalJSONFFLexer fast json unmarshall - template ffjson
+func (j *AssetFundFeePoolOperation) UnmarshalJSONFFLexer(fs *fflib.FFLexer, state fflib.FFParseState) error {
+	var err error
+	currentKey := ffjtAssetFundFeePoolOperationbase
+	_ = currentKey
+	tok := fflib.FFTok_init
+	wantedTok := fflib.FFTok_init
+
+mainparse:
+	for {
+		tok = fs.Scan()
+		//	println(fmt.Sprintf("debug: tok: %v  state: %v", tok, state))
+		if tok == fflib.FFTok_error {
+			goto tokerror
+		}
+
+		switch state {
+
+		case fflib.FFParse_map_start:
+			if tok != fflib.FFTok_left_bracket {
+				wantedTok = fflib.FFTok_left_bracket
+				goto wrongtokenerror
+			}
+			state = fflib.FFParse_want_key
+			continue
+
+		case fflib.FFParse_after_value:
+			if tok == fflib.FFTok_comma {
+				state = fflib.FFParse_want_key
+			} else if tok == fflib.FFTok_right_bracket {
+				goto done
+			} else {
+				wantedTok = fflib.FFTok_comma
+				goto wrongtokenerror
+			}
+
+		case fflib.FFParse_want_key:
+			// json {} ended. goto exit. woo.
+			if tok == fflib.FFTok_right_bracket {
+				goto done
+			}
