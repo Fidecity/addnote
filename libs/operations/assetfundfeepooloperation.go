40e52862c1118bc6ab8f042ab7e66296881acec9
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-05-06 18:57:24 +0000

    save config

--- libs/operations/assetfundfeepooloperation.go
+++ libs/operations/assetfundfeepooloperation.go
@@ -36,4 +36,21 @@ func (p AssetFundFeePoolOperation) Marshal(enc *util.TypeEncoder) error {
 		return errors.Annotate(err, "encode fee")
 	}
 
-	if err := enc.Encode(p.FromAccount); er
+	if err := enc.Encode(p.FromAccount); err != nil {
+		return errors.Annotate(err, "encode new options")
+	}
+
+	if err := enc.Encode(p.AssetID); err != nil {
+		return errors.Annotate(err, "encode asset id")
+	}
+
+	if err := enc.Encode(p.Amount); err != nil {
+		return errors.Annotate(err, "encode amount")
+	}
+
+	if err := enc.Encode(p.Extensions); err != nil {
+		return errors.Annotate(err, "encode extensions")
+	}
+
+	return nil
+}
