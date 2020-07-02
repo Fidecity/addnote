b42d6aeea0822d4cd4c7b44d727b87970d6e4538
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-07-02 16:57:19 +0000

    Updates:

--- libs/operations/assetglobalsettleoperation.go
+++ libs/operations/assetglobalsettleoperation.go
@@ -24,4 +24,27 @@ type AssetGlobalSettleOperation struct {
 }
 
 func (p AssetGlobalSettleOperation) Type() types.OperationType {
-	re
+	return types.OperationTypeAssetGlobalSettle
+}
+
+func (p AssetGlobalSettleOperation) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.Encode(int8(p.Type())); err != nil {
+		return errors.Annotate(err, "encode OperationType")
+	}
+	if err := enc.Encode(p.Fee); err != nil {
+		return errors.Annotate(err, "encode Fee")
+	}
+	if err := enc.Encode(p.Issuer); err != nil {
+		return errors.Annotate(err, "encode Issuer")
+	}
+	if err := enc.Encode(p.AssetToSettle); err != nil {
+		return errors.Annotate(err, "encode AssetToSettle")
+	}
+	if err := enc.Encode(p.SettlePrice); err != nil {
+		return errors.Annotate(err, "encode SettlePrice")
+	}
+	if err := enc.Encode(p.Extensions); err != nil {
+		return errors.Annotate(err, "encode Extensions")
+	}
+	return nil
+}
