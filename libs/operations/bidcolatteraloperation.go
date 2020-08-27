e7cbcfa177e67fce309f30efe36d660a3ffc2c08
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-08-27 20:15:45 +0000

    Update exp.jl

--- libs/operations/bidcolatteraloperation.go
+++ libs/operations/bidcolatteraloperation.go
@@ -28,4 +28,29 @@ func (p BidCollateralOperation) Type() types.OperationType {
 }
 
 func (p BidCollateralOperation) Marshal(enc *util.TypeEncoder) error {
-	if err := enc.En
+	if err := enc.Encode(int8(p.Type())); err != nil {
+		return errors.Annotate(err, "encode OperationType")
+	}
+
+	if err := enc.Encode(p.Fee); err != nil {
+		return errors.Annotate(err, "encode fee")
+	}
+
+	if err := enc.Encode(p.Bidder); err != nil {
+		return errors.Annotate(err, "encode Bidder")
+	}
+
+	if err := enc.Encode(p.AdditionalCollateral); err != nil {
+		return errors.Annotate(err, "encode AdditionalCollateral")
+	}
+
+	if err := enc.Encode(p.DebtCovered); err != nil {
+		return errors.Annotate(err, "encode DebtCovered")
+	}
+
+	if err := enc.Encode(p.Extensions); err != nil {
+		return errors.Annotate(err, "encode Extensions")
+	}
+
+	return nil
+}
---
+++ libs/operations/callorderupdateoperation.go
@@ -0,0 +1,12 @@
+package operations
+
+//go:generate ffjson $GOFILE
+
+import (
+	"github.com/Assetsadapter/whitecoin-adapter/libs/types"
+	"github.com/Assetsadapter/whitecoin-adapter/libs/util"
+	"github.com/juju/errors"
+)
+
+func init() {
+	types.OperationMap[types.OperationTypeCallOrderUpdate] = 
