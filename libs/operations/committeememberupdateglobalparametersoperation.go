8a6b32e9c30f5e715379697956ba68a95fff62b1
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-09-20 14:38:07 +0000

    Doc.

--- libs/operations/committeememberupdateglobalparametersoperation.go
+++ libs/operations/committeememberupdateglobalparametersoperation.go
@@ -31,4 +31,21 @@ type ChainParameters struct {
 	MaxPredicateOpcode               types.UInt16      `json:"max_predicate_opcode"`
 	MaximumAuthorityMembership       types.UInt16      `json:"maximum_authority_membership"`
 	MaximumCommitteeCount            types.UInt16      `json:"maximum_committee_count"`
-	MaximumWitnessC
+	MaximumWitnessCount              types.UInt16      `json:"maximum_witness_count"`
+	NetworkPercentOfFee              types.UInt16      `json:"network_percent_of_fee"`
+	ReservePercentOfFee              types.UInt16      `json:"reserve_percent_of_fee"`
+	CashbackVestingPeriodSeconds     types.UInt32      `json:"cashback_vesting_period_seconds"`
+	CommitteeProposalReviewPeriod    types.UInt32      `json:"committee_proposal_review_period"`
+	WitnessPayVestingSeconds         types.UInt32      `json:"witness_pay_vesting_seconds"`
+	MaximumProposalLifetime          types.UInt32      `json:"maximum_proposal_lifetime"`
+	MaximumTimeUntilExpiration       types.UInt32      `json:"maximum_time_until_expiration"`
+	MaximumTransactionSize           types.UInt32      `json:"maximum_transaction_size"`
+	MaintenanceInterval              types.UInt32      `json:"maintenance_interval"`
+	MaximumBlockSize                 types.UInt32      `json:"maximum_block_size"`
+	CashbackVestingThreshold         types.Int64       `json:"cashback_vesting_threshold"`
+	WitnessPayPerBlock               types.Int64       `json:"witness_pay_per_block"`
+	WorkerBudgetPerDay               types.Int64       `json:"worker_budget_per_day"`
+	FeeLiquidationThreshold          types.Int64       `json:"fee_liquidation_threshold"`
+}
+
+func (p ChainParameters) Marshal(enc *util.TypeEncoder) error 
