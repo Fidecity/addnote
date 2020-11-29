68a1168e3ffa179e126979aceee6891d3b55bfe9
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-11-29 20:50:41 +0000

    Fixed image link

--- libs/operations/committeememberupdateoperation_ffjson.go
+++ libs/operations/committeememberupdateoperation_ffjson.go
@@ -236,4 +236,63 @@ mainparse:
 			continue
 		case fflib.FFParse_want_value:
 
-			if tok == fflib.FFTok_left_brace || tok == fflib.FFTok_left_bracket || 
+			if tok == fflib.FFTok_left_brace || tok == fflib.FFTok_left_bracket || tok == fflib.FFTok_integer || tok == fflib.FFTok_double || tok == fflib.FFTok_string || tok == fflib.FFTok_bool || tok == fflib.FFTok_null {
+				switch currentKey {
+
+				case ffjtCommitteeMemberUpdateOperationCommitteeMember:
+					goto handle_CommitteeMember
+
+				case ffjtCommitteeMemberUpdateOperationCommitteeMemberAccount:
+					goto handle_CommitteeMemberAccount
+
+				case ffjtCommitteeMemberUpdateOperationNewURL:
+					goto handle_NewURL
+
+				case ffjtCommitteeMemberUpdateOperationFee:
+					goto handle_Fee
+
+				case ffjtCommitteeMemberUpdateOperationnosuchkey:
+					err = fs.SkipField(tok)
+					if err != nil {
+						return fs.WrapErr(err)
+					}
+					state = fflib.FFParse_after_value
+					goto mainparse
+				}
+			} else {
+				goto wantedvalue
+			}
+		}
+	}
+
+handle_CommitteeMember:
+
+	/* handler: j.CommitteeMember type=types.CommitteeMember kind=struct quoted=false*/
+
+	{
+		/* Falling back. type=types.CommitteeMember kind=struct */
+		tbuf, err := fs.CaptureField(tok)
+		if err != nil {
+			return fs.WrapErr(err)
+		}
+
+		err = json.Unmarshal(tbuf, &j.CommitteeMember)
+		if err != nil {
+			return fs.WrapErr(err)
+		}
+	}
+
+	state = fflib.FFParse_after_value
+	goto mainparse
+
+handle_CommitteeMemberAccount:
+
+	/* handler: j.CommitteeMemberAccount type=types.AccountID kind=struct quoted=false*/
+
+	{
+		if tok == fflib.FFTok_null {
+
+		} else {
+
+			tbuf, err := fs.CaptureField(tok)
+			if err != n
