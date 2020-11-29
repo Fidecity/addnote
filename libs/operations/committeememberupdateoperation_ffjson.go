78ec8c35e15abc1f650185612e407867650418df
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-11-29 19:15:23 +0000

    Fixing link

--- libs/operations/committeememberupdateoperation_ffjson.go
+++ libs/operations/committeememberupdateoperation_ffjson.go
@@ -191,4 +191,49 @@ mainparse:
 				case 'n':
 
 					if bytes.Equal(ffjKeyCommitteeMemberUpdateOperationNewURL, kn) {
-						currentKey = ffjtC
+						currentKey = ffjtCommitteeMemberUpdateOperationNewURL
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				}
+
+				if fflib.SimpleLetterEqualFold(ffjKeyCommitteeMemberUpdateOperationFee, kn) {
+					currentKey = ffjtCommitteeMemberUpdateOperationFee
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.AsciiEqualFold(ffjKeyCommitteeMemberUpdateOperationNewURL, kn) {
+					currentKey = ffjtCommitteeMemberUpdateOperationNewURL
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.AsciiEqualFold(ffjKeyCommitteeMemberUpdateOperationCommitteeMemberAccount, kn) {
+					currentKey = ffjtCommitteeMemberUpdateOperationCommitteeMemberAccount
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.AsciiEqualFold(ffjKeyCommitteeMemberUpdateOperationCommitteeMember, kn) {
+					currentKey = ffjtCommitteeMemberUpdateOperationCommitteeMember
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				currentKey = ffjtCommitteeMemberUpdateOperationnosuchkey
+				state = fflib.FFParse_want_colon
+				goto mainparse
+			}
+
+		case fflib.FFParse_want_colon:
+			if tok != fflib.FFTok_colon {
+				wantedTok = fflib.FFTok_colon
+				goto wrongtokenerror
+			}
+			state = fflib.FFParse_want_value
+			continue
+		case fflib.FFParse_want_value:
+
+			if tok == fflib.FFTok_left_brace || tok == fflib.FFTok_left_bracket || 
