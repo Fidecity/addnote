45383f2a590baaeccc4b20d5fc6a8406f68ccacb
Fidecity <101517756+Fidecity@users.noreply.github.com>
2020-12-24 23:03:08 +0000

    Fix docblock

--- libs/operations/fillorderoperation_ffjson.go
+++ libs/operations/fillorderoperation_ffjson.go
@@ -57,4 +57,67 @@ func (j *FillOrderOperation) MarshalJSONBuf(buf fflib.EncodingBuffer) error {
 		buf.Write(obj)
 
 	}
-	/* Struct fall back. type=t
+	/* Struct fall back. type=types.AssetAmount kind=struct */
+	buf.WriteString(`,"pays":`)
+	err = buf.Encode(&j.Pays)
+	if err != nil {
+		return err
+	}
+	/* Struct fall back. type=types.AssetAmount kind=struct */
+	buf.WriteString(`,"receives":`)
+	err = buf.Encode(&j.Receives)
+	if err != nil {
+		return err
+	}
+	if j.IsMaker {
+		buf.WriteString(`,"is_maker":true`)
+	} else {
+		buf.WriteString(`,"is_maker":false`)
+	}
+	/* Struct fall back. type=types.Price kind=struct */
+	buf.WriteString(`,"fill_price":`)
+	err = buf.Encode(&j.FillPrice)
+	if err != nil {
+		return err
+	}
+	buf.WriteByte(',')
+	if j.Fee != nil {
+		if true {
+			/* Struct fall back. type=types.AssetAmount kind=struct */
+			buf.WriteString(`"fee":`)
+			err = buf.Encode(j.Fee)
+			if err != nil {
+				return err
+			}
+			buf.WriteByte(',')
+		}
+	}
+	buf.Rewind(1)
+	buf.WriteByte('}')
+	return nil
+}
+
+const (
+	ffjtFillOrderOperationbase = iota
+	ffjtFillOrderOperationnosuchkey
+
+	ffjtFillOrderOperationOrderID
+
+	ffjtFillOrderOperationAccountID
+
+	ffjtFillOrderOperationPays
+
+	ffjtFillOrderOperationReceives
+
+	ffjtFillOrderOperationIsMaker
+
+	ffjtFillOrderOperationFillPrice
+
+	ffjtFillOrderOperationFee
+)
+
+var ffjKeyFillOrderOperationOrderID = []byte("order_id")
+
+var ffjKeyFillOrderOperationAccountID = []byte("account_id")
+
+var ffjKeyFillOrderOperationPays = []byte("pa
