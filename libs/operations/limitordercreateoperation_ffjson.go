381a9502075a4fddf556b94a433dcbad23410b99
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-01-09 09:59:58 +0000

    no idea

--- libs/operations/limitordercreateoperation_ffjson.go
+++ libs/operations/limitordercreateoperation_ffjson.go
@@ -170,4 +170,55 @@ mainparse:
 		case fflib.FFParse_after_value:
 			if tok == fflib.FFTok_comma {
 				state = fflib.FFParse_want_key
-			} else
+			} else if tok == fflib.FFTok_right_bracket {
+				goto done
+			} else {
+				wantedTok = fflib.FFTok_comma
+				goto wrongtokenerror
+			}
+
+		case fflib.FFParse_want_key:
+			// json {} ended. goto exit. woo.
+			if tok == fflib.FFTok_right_bracket {
+				goto done
+			}
+			if tok != fflib.FFTok_string {
+				wantedTok = fflib.FFTok_string
+				goto wrongtokenerror
+			}
+
+			kn := fs.Output.Bytes()
+			if len(kn) <= 0 {
+				// "" case. hrm.
+				currentKey = ffjtLimitOrderCreateOperationnosuchkey
+				state = fflib.FFParse_want_colon
+				goto mainparse
+			} else {
+				switch kn[0] {
+
+				case 'a':
+
+					if bytes.Equal(ffjKeyLimitOrderCreateOperationAmountToSell, kn) {
+						currentKey = ffjtLimitOrderCreateOperationAmountToSell
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				case 'e':
+
+					if bytes.Equal(ffjKeyLimitOrderCreateOperationExpiration, kn) {
+						currentKey = ffjtLimitOrderCreateOperationExpiration
+						state = fflib.FFParse_want_colon
+						goto mainparse
+
+					} else if bytes.Equal(ffjKeyLimitOrderCreateOperationExtensions, kn) {
+						currentKey = ffjtLimitOrderCreateOperationExtensions
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				case 'f':
+
+					if bytes.Equal(ffjKeyLimitOrderCreateOperationFillOrKill, kn) {
+						currentKey = ffjtLimitOrderCreateOperationFillOrKill
+						state = ffl
