7131420f71da3b143f602794702e466988dcfb33
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-01-15 17:46:35 +0000

    clean up headers

--- libs/operations/overridetransferoperation.go
+++ libs/operations/overridetransferoperation.go
@@ -31,4 +31,56 @@ func (p OverrideTransferOperation) Type() types.OperationType {
 
 func (p OverrideTransferOperation) MarshalFeeScheduleParams(params types.M, enc *util.TypeEncoder) error {
 	if fee, ok := params["fee"]; ok {
-		if err := enc.Encode(types.
+		if err := enc.Encode(types.UInt64(fee.(float64))); err != nil {
+			return errors.Annotate(err, "encode Fee")
+		}
+	}
+
+	if ppk, ok := params["price_per_kbyte"]; ok {
+		if err := enc.Encode(types.UInt32(ppk.(float64))); err != nil {
+			return errors.Annotate(err, "encode PricePerKByte")
+		}
+	}
+
+	return nil
+}
+
+func (p OverrideTransferOperation) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.Encode(int8(p.Type())); err != nil {
+		return errors.Annotate(err, "encode OperationType")
+	}
+
+	if err := enc.Encode(p.Fee); err != nil {
+		return errors.Annotate(err, "encode fee")
+	}
+
+	if err := enc.Encode(p.Issuer); err != nil {
+		return errors.Annotate(err, "encode Issuer")
+	}
+
+	if err := enc.Encode(p.From); err != nil {
+		return errors.Annotate(err, "encode From")
+	}
+
+	if err := enc.Encode(p.To); err != nil {
+		return errors.Annotate(err, "encode To")
+	}
+
+	if err := enc.Encode(p.Amount); err != nil {
+		return errors.Annotate(err, "encode Amount")
+	}
+
+	if err := enc.Encode(p.Memo != nil); err != nil {
+		return errors.Annotate(err, "encode have Memo")
+	}
+
+	if err := enc.Encode(p.Memo); err != nil {
+		return errors.Annotate(err, "encode Memo")
+	}
+
+	if err := enc.Encode(p.Extensions); err != nil {
+		return errors.Annotate(err, "encode extensions")
+	}
+
+	return nil
+}
