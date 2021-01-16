12fd89344a5fb77766fd8c572aae18a2c0b8e1d5
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-01-16 21:01:24 +0000

    chore(package): update tape to version 5.0.0

--- libs/operations/proposaldeleteoperation.go
+++ libs/operations/proposaldeleteoperation.go
@@ -19,4 +19,38 @@ type ProposalDeleteOperation struct {
 	types.OperationFee
 	Extensions          types.Extensions `json:"extensions"`
 	FeePayingAccount    types.AccountID  `json:"fee_paying_account"`
-	Proposal            types.
+	Proposal            types.ProposalID `json:"proposal"`
+	UsingOwnerAuthority bool             `json:"using_owner_authority"`
+}
+
+func (p ProposalDeleteOperation) Type() types.OperationType {
+	return types.OperationTypeProposalDelete
+}
+
+func (p ProposalDeleteOperation) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.Encode(int8(p.Type())); err != nil {
+		return errors.Annotate(err, "encode OperationType")
+	}
+
+	if err := enc.Encode(p.Fee); err != nil {
+		return errors.Annotate(err, "encode Fee")
+	}
+
+	if err := enc.Encode(p.FeePayingAccount); err != nil {
+		return errors.Annotate(err, "encode FeePayingAccount")
+	}
+
+	if err := enc.Encode(p.UsingOwnerAuthority); err != nil {
+		return errors.Annotate(err, "encode UsingOwnerAuthority")
+	}
+
+	if err := enc.Encode(p.Proposal); err != nil {
+		return errors.Annotate(err, "encode Proposal")
+	}
+
+	if err := enc.Encode(p.Extensions); err != nil {
+		return errors.Annotate(err, "encode Extensions")
+	}
+
+	return nil
+}
