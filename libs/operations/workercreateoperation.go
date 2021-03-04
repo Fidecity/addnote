1919ff71811d48fa4cd88ada1a596e09bfc65012
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-03-04 11:42:42 +0000

    Add more test cases

--- libs/operations/workercreateoperation.go
+++ libs/operations/workercreateoperation.go
@@ -19,4 +19,48 @@ type WorkerCreateOperation struct {
 	types.OperationFee
 	DailyPay      types.UInt64            `json:"daily_pay"`
 	Initializer   types.WorkerInitializer `json:"initializer"`
-	Name          strin
+	Name          string                  `json:"name"`
+	Owner         types.AccountID         `json:"owner"`
+	URL           string                  `json:"url"`
+	WorkBeginDate types.Time              `json:"work_begin_date"`
+	WorkEndDate   types.Time              `json:"work_end_date"`
+}
+
+func (p WorkerCreateOperation) Type() types.OperationType {
+	return types.OperationTypeWorkerCreate
+}
+
+func (p WorkerCreateOperation) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.Encode(int8(p.Type())); err != nil {
+		return errors.Annotate(err, "encode OperationType")
+	}
+
+	if err := enc.Encode(p.Fee); err != nil {
+		return errors.Annotate(err, "encode fee")
+	}
+
+	if err := enc.Encode(p.Owner); err != nil {
+		return errors.Annotate(err, "encode Owner")
+	}
+
+	if err := enc.Encode(p.WorkBeginDate); err != nil {
+		return errors.Annotate(err, "encode WorkBeginDate")
+	}
+
+	if err := enc.Encode(p.WorkEndDate); err != nil {
+		return errors.Annotate(err, "encode WorkEndDate")
+	}
+
+	if err := enc.Encode(p.DailyPay); err != nil {
+		return errors.Annotate(err, "encode DailyPay")
+	}
+
+	if err := enc.Encode(p.Name); err != nil {
+		return errors.Annotate(err, "encode Name")
+	}
+
+	if err := enc.Encode(p.URL); err != nil {
+		return errors.Annotate(err, "encode URL")
+	}
+
+	if err := enc.Enco
