1298dde754c84b94b2e8566c270d8f4842970272
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-04-04 08:16:14 +0000

    New colors

--- libs/types/account_ffjson.go
+++ libs/types/account_ffjson.go
@@ -1121,4 +1121,64 @@ handle_BlacklistingAccounts:
 	state = fflib.FFParse_after_value
 	goto mainparse
 
-handle_Whiteliste
+handle_WhitelistedAccounts:
+
+	/* handler: j.WhitelistedAccounts type=types.AccountIDs kind=slice quoted=false*/
+
+	{
+
+		{
+			if tok != fflib.FFTok_left_brace && tok != fflib.FFTok_null {
+				return fs.WrapErr(fmt.Errorf("cannot unmarshal %s into Go value for AccountIDs", tok))
+			}
+		}
+
+		if tok == fflib.FFTok_null {
+			j.WhitelistedAccounts = nil
+		} else {
+
+			j.WhitelistedAccounts = []AccountID{}
+
+			wantVal := true
+
+			for {
+
+				var tmpJWhitelistedAccounts AccountID
+
+				tok = fs.Scan()
+				if tok == fflib.FFTok_error {
+					goto tokerror
+				}
+				if tok == fflib.FFTok_right_brace {
+					break
+				}
+
+				if tok == fflib.FFTok_comma {
+					if wantVal == true {
+						// TODO(pquerna): this isn't an ideal error message, this handles
+						// things like [,,,] as an array value.
+						return fs.WrapErr(fmt.Errorf("wanted value token, but got token: %v", tok))
+					}
+					continue
+				} else {
+					wantVal = true
+				}
+
+				/* handler: tmpJWhitelistedAccounts type=types.AccountID kind=struct quoted=false*/
+
+				{
+					if tok == fflib.FFTok_null {
+
+					} else {
+
+						tbuf, err := fs.CaptureField(tok)
+						if err != nil {
+							return fs.WrapErr(err)
+						}
+
+						err = tmpJWhitelistedAccounts.UnmarshalJSON(tbuf)
+						if err != nil {
+							return fs.WrapErr(err)
+						}
+					}
+					state = ff
