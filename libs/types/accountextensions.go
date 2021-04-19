110489b55f24212e459bda0ad8426c8cf03a3e6b
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-04-19 23:01:46 +0000

    chore: update AMSF

--- libs/types/accountextensions.go
+++ libs/types/accountextensions.go
@@ -66,4 +66,56 @@ func (p AccountCreateExtensions) Length() int {
 }
 
 func (p AccountCreateExtensions) Marshal(enc *util.TypeEncoder) error {
-	if err := enc.EncodeUVarint(
+	if err := enc.EncodeUVarint(uint64(p.Length())); err != nil {
+		return errors.Annotate(err, "encode length")
+	}
+
+	if p.NullExt != nil {
+		if err := enc.Encode(p.NullExt); err != nil {
+			return errors.Annotate(err, "encode NullExt")
+		}
+	}
+
+	if p.OwnerSpecialAuthority != nil {
+		if err := enc.Encode(p.OwnerSpecialAuthority); err != nil {
+			return errors.Annotate(err, "encode OwnerSpecialAuthority")
+		}
+	}
+
+	if p.ActiveSpecialAuthority != nil {
+		if err := enc.Encode(p.ActiveSpecialAuthority); err != nil {
+			return errors.Annotate(err, "encode ActiveSpecialAuthority")
+		}
+	}
+
+	if p.BuybackOptions != nil {
+		if err := enc.Encode(p.BuybackOptions); err != nil {
+			return errors.Annotate(err, "encode BuybackOptions")
+		}
+	}
+
+	return nil
+}
+
+type AccountUpdateExtensions struct {
+	NullExt                *NullExtension          `json:"null_ext,omitempty"`
+	OwnerSpecialAuthority  *OwnerSpecialAuthority  `json:"owner_special_authority,omitempty"`
+	ActiveSpecialAuthority *ActiveSpecialAuthority `json:"active_special_authority,omitempty"`
+}
+
+func (p AccountUpdateExtensions) Length() int {
+	fields := 0
+	if p.NullExt != nil {
+		fields++
+	}
+	if p.OwnerSpecialAuthority != nil {
+		fields++
+	}
+	if p.ActiveSpecialAuthority != nil {
+		fields++
+	}
+
+	return fields
+}
+
+func (p AccountUpdateExtensions) Marshal(enc *util.TypeEncoder
