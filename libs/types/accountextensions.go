144feb48ac76f9af7e2db23ec90b73898d850828
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-04-19 10:58:58 +0000

    Android-01 new

--- libs/types/accountextensions.go
+++ libs/types/accountextensions.go
@@ -19,4 +19,51 @@ func (p NullExtension) Marshal(enc *util.TypeEncoder) error {
 
 type BuybackOptions struct {
 	AssetToBuy       AssetID   `json:"asset_to_buy"`
-	AssetToBuyIssuer Accoun
+	AssetToBuyIssuer AccountID `json:"asset_to_buy_issuer"`
+	Markets          AssetIDs  `json:"markets"`
+}
+
+func (p BuybackOptions) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.Encode(uint8(AccountCreateExtensionsBuyback)); err != nil {
+		return errors.Annotate(err, "encode AccountCreateExtensionsBuyback")
+	}
+	if err := enc.Encode(p.AssetToBuy); err != nil {
+		return errors.Annotate(err, "encode AssetToBuy")
+	}
+	if err := enc.Encode(p.AssetToBuyIssuer); err != nil {
+		return errors.Annotate(err, "encode AssetToBuyIssuer")
+	}
+	if err := enc.Encode(p.Markets); err != nil {
+		return errors.Annotate(err, "encode Markets")
+	}
+
+	return nil
+}
+
+type AccountCreateExtensions struct {
+	NullExt                *NullExtension          `json:"null_ext,omitempty"`
+	OwnerSpecialAuthority  *OwnerSpecialAuthority  `json:"owner_special_authority,omitempty"`
+	ActiveSpecialAuthority *ActiveSpecialAuthority `json:"active_special_authority,omitempty"`
+	BuybackOptions         *BuybackOptions         `json:"buyback_options,omitempty"`
+}
+
+func (p AccountCreateExtensions) Length() int {
+	fields := 0
+	if p.NullExt != nil {
+		fields++
+	}
+	if p.OwnerSpecialAuthority != nil {
+		fields++
+	}
+	if p.ActiveSpecialAuthority != nil {
+		fields++
+	}
+	if p.BuybackOptions != nil {
+		fields++
+	}
+
+	return fields
+}
+
+func (p AccountCreateExtensions) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.EncodeUVarint(
