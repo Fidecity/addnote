bd2911f4cda6e707c515bcf2400ee769c69279ba
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-07-24 05:48:31 +0000

    Build fixed

--- libs/types/block_ffjson.go
+++ libs/types/block_ffjson.go
@@ -971,4 +971,60 @@ mainparse:
 				goto wrongtokenerror
 			}
 			state = fflib.FFParse_want_key
-			
+			continue
+
+		case fflib.FFParse_after_value:
+			if tok == fflib.FFTok_comma {
+				state = fflib.FFParse_want_key
+			} else if tok == fflib.FFTok_right_bracket {
+				goto done
+			} else {
+				wantedTok = fflib.FFTok_comma
+				goto wrongtokenerror
+			}
+
+		case fflib.FFParse_want_key:
+			// json {} ended. goto exit. woo.
+			if tok == fflib.FFTok_right_bracket {
+				goto done
+			}
+			if tok != fflib.FFTok_string {
+				wantedTok = fflib.FFTok_string
+				goto wrongtokenerror
+			}
+
+			kn := fs.Output.Bytes()
+			if len(kn) <= 0 {
+				// "" case. hrm.
+				currentKey = ffjtBlockHeadernosuchkey
+				state = fflib.FFParse_want_colon
+				goto mainparse
+			} else {
+				switch kn[0] {
+
+				case 'e':
+
+					if bytes.Equal(ffjKeyBlockHeaderExtensions, kn) {
+						currentKey = ffjtBlockHeaderExtensions
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				case 'p':
+
+					if bytes.Equal(ffjKeyBlockHeaderPrevious, kn) {
+						currentKey = ffjtBlockHeaderPrevious
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				case 't':
+
+					if bytes.Equal(ffjKeyBlockHeaderTransactionMerkleRoot, kn) {
+						currentKey = ffjtBlockHeaderTransactionMerkleRoot
+						state = fflib.FFParse_want_colon
+						goto mainparse
+
+					} else if bytes.Equal(ffjKeyBlockHeaderTimeStamp, kn) {
+						currentKey = ffjtBlockHeaderTimeStamp
+						state = fflib.FF
