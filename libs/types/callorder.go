ffa3bafe84d009c557586a9c8b26ab3e07ae27bc
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-08-07 20:43:44 +0000

    USB: xhci: correctly enable interrupts

--- libs/types/callorder.go
+++ libs/types/callorder.go
@@ -4,4 +4,9 @@ package types
 
 type CallOrders []CallOrder
 type CallOrder struct {
-	ID         CallOrder
+	ID         CallOrderID `json:"id"`
+	Borrower   AccountID   `json:"borrower"`
+	Collateral Int64       `json:"collateral"`
+	Debt       Int64       `json:"debt"`
+	CallPrice  Price       `json:"call_price"`
+}
