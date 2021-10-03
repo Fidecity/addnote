d5ca182210d0db36bddf4e7b71ef0f8ff62d32d9
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-10-03 19:18:58 +0000

    msm: mdss: Adding lcd notifier

--- libs/types/fullaccount_ffjson.go
+++ libs/types/fullaccount_ffjson.go
@@ -451,3 +451,52 @@ mainparse:
 
 				if fflib.EqualFoldRight(ffjKeyAccountInfoBalances, kn) {
 					currentKey = ffjtAccountInfoBalances
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.EqualFoldRight(ffjKeyAccountInfoCashbackBalance, kn) {
+					currentKey = ffjtAccountInfoCashbackBalance
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.AsciiEqualFold(ffjKeyAccountInfoLifetimeReferrerName, kn) {
+					currentKey = ffjtAccountInfoLifetimeReferrerName
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.AsciiEqualFold(ffjKeyAccountInfoReferrerName, kn) {
+					currentKey = ffjtAccountInfoReferrerName
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.EqualFoldRight(ffjKeyAccountInfoRegistrarName, kn) {
+					currentKey = ffjtAccountInfoRegistrarName
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.SimpleLetterEqualFold(ffjKeyAccountInfoAccount, kn) {
+					currentKey = ffjtAccountInfoAccount
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				currentKey = ffjtAccountInfonosuchkey
+				state = fflib.FFParse_want_colon
+				goto mainparse
+			}
+
+		case fflib.FFParse_want_colon:
+			if tok != fflib.FFTok_colon {
+				wantedTok = fflib.FFTok_colon
+				goto wrongtokenerror
+			}
+			state = fflib.FFParse_want_value
+			continue
+		case fflib.FFParse_want_value:
+
+			if tok == fflib.FFTok_left_brace || tok == fflib.FFTo
