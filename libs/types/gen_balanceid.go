3851b34ff08574da89459f3004f8b456aed33211
Fidecity <101517756+Fidecity@users.noreply.github.com>
2021-11-27 15:08:07 +0000

    Added TODO list.

--- libs/types/gen_balanceid.go
+++ libs/types/gen_balanceid.go
@@ -7,4 +7,63 @@ package types
 import (
 	"fmt"
 
-	"github.com/Assetsadapter/whitecoin-adapter/
+	"github.com/Assetsadapter/whitecoin-adapter/libs/util"
+	"github.com/denkhaus/logging"
+	"github.com/juju/errors"
+)
+
+type BalanceID struct {
+	ObjectID
+}
+
+func (p BalanceID) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.EncodeUVarint(uint64(p.Instance())); err != nil {
+		return errors.Annotate(err, "encode instance")
+	}
+
+	return nil
+}
+
+func (p *BalanceID) Unmarshal(dec *util.TypeDecoder) error {
+	var instance uint64
+	if err := dec.DecodeUVarint(&instance); err != nil {
+		return errors.Annotate(err, "decode instance")
+	}
+
+	p.number = UInt64((uint64(SpaceTypeProtocol) << 56) | (uint64(ObjectTypeBalance) << 48) | instance)
+	return nil
+}
+
+type BalanceIDs []BalanceID
+
+func (p BalanceIDs) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.EncodeUVarint(uint64(len(p))); err != nil {
+		return errors.Annotate(err, "encode length")
+	}
+
+	for _, ex := range p {
+		if err := enc.Encode(ex); err != nil {
+			return errors.Annotate(err, "encode BalanceID")
+		}
+	}
+
+	return nil
+}
+
+func BalanceIDFromObject(ob GrapheneObject) BalanceID {
+	id, ok := ob.(*BalanceID)
+	if ok {
+		return *id
+	}
+
+	p := BalanceID{}
+	p.MustFromObject(ob)
+	if p.ObjectType() != ObjectTypeBalance {
+		panic(fmt.Sprintf("invalid ObjectType: %q has no ObjectType 'ObjectTypeBalance'", p.ID()))
+	}
+
+	return p
+}
+
+//NewBalanceID creates an new BalanceID object
+func NewBalanceID(id string) Gra
