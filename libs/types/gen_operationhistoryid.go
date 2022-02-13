7a4916017921b272622eca4a695b1917fed752a8
Fidecity <101517756+Fidecity@users.noreply.github.com>
2022-02-13 20:40:03 +0000

    Update Protocols_Details.md

--- libs/types/gen_operationhistoryid.go
+++ libs/types/gen_operationhistoryid.go
@@ -16,4 +16,54 @@ type OperationHistoryID struct {
 	ObjectID
 }
 
-func (p OperationHis
+func (p OperationHistoryID) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.EncodeUVarint(uint64(p.Instance())); err != nil {
+		return errors.Annotate(err, "encode instance")
+	}
+
+	return nil
+}
+
+func (p *OperationHistoryID) Unmarshal(dec *util.TypeDecoder) error {
+	var instance uint64
+	if err := dec.DecodeUVarint(&instance); err != nil {
+		return errors.Annotate(err, "decode instance")
+	}
+
+	p.number = UInt64((uint64(SpaceTypeProtocol) << 56) | (uint64(ObjectTypeOperationHistory) << 48) | instance)
+	return nil
+}
+
+type OperationHistoryIDs []OperationHistoryID
+
+func (p OperationHistoryIDs) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.EncodeUVarint(uint64(len(p))); err != nil {
+		return errors.Annotate(err, "encode length")
+	}
+
+	for _, ex := range p {
+		if err := enc.Encode(ex); err != nil {
+			return errors.Annotate(err, "encode OperationHistoryID")
+		}
+	}
+
+	return nil
+}
+
+func OperationHistoryIDFromObject(ob GrapheneObject) OperationHistoryID {
+	id, ok := ob.(*OperationHistoryID)
+	if ok {
+		return *id
+	}
+
+	p := OperationHistoryID{}
+	p.MustFromObject(ob)
+	if p.ObjectType() != ObjectTypeOperationHistory {
+		panic(fmt.Sprintf("invalid ObjectType: %q has no ObjectType 'ObjectTypeOperationHistory'", p.ID()))
+	}
+
+	return p
+}
+
+//NewOperationHistoryID creates an new OperationHistoryID object
+func NewOperationHistoryID(id string) GrapheneObjec
