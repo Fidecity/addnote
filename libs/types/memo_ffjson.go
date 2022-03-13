bc642af87352bc1c5884028484d64a11f995dc1e
Fidecity <101517756+Fidecity@users.noreply.github.com>
2022-03-13 15:34:21 +0000

    minor improvements.

--- libs/types/memo_ffjson.go
+++ libs/types/memo_ffjson.go
@@ -78,4 +78,61 @@ const (
 
 	ffjtMemoFrom
 
-	ffj
+	ffjtMemoTo
+
+	ffjtMemoNonce
+
+	ffjtMemoMessage
+)
+
+var ffjKeyMemoFrom = []byte("from")
+
+var ffjKeyMemoTo = []byte("to")
+
+var ffjKeyMemoNonce = []byte("nonce")
+
+var ffjKeyMemoMessage = []byte("message")
+
+// UnmarshalJSON umarshall json - template of ffjson
+func (j *Memo) UnmarshalJSON(input []byte) error {
+	fs := fflib.NewFFLexer(input)
+	return j.UnmarshalJSONFFLexer(fs, fflib.FFParse_map_start)
+}
+
+// UnmarshalJSONFFLexer fast json unmarshall - template ffjson
+func (j *Memo) UnmarshalJSONFFLexer(fs *fflib.FFLexer, state fflib.FFParseState) error {
+	var err error
+	currentKey := ffjtMemobase
+	_ = currentKey
+	tok := fflib.FFTok_init
+	wantedTok := fflib.FFTok_init
+
+mainparse:
+	for {
+		tok = fs.Scan()
+		//	println(fmt.Sprintf("debug: tok: %v  state: %v", tok, state))
+		if tok == fflib.FFTok_error {
+			goto tokerror
+		}
+
+		switch state {
+
+		case fflib.FFParse_map_start:
+			if tok != fflib.FFTok_left_bracket {
+				wantedTok = fflib.FFTok_left_bracket
+				goto wrongtokenerror
+			}
+			state = fflib.FFParse_want_key
+			continue
+
+		case fflib.FFParse_after_value:
+			if tok == fflib.FFTok_comma {
+				state = fflib.FFParse_want_key
+			} else if tok == fflib.FFTok_right_bracket {
+				goto done
+			} else {
+				wantedTok = fflib.FFTok_comma
+				goto wrongtokenerror
+			}
+
+		case fflib.FFParse_want_ke
