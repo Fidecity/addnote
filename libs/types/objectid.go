2208c6c495c573e4e3a2b8f0a57e55bb3fd1f2d2
Fidecity <101517756+Fidecity@users.noreply.github.com>
2022-04-24 08:20:25 +0000

    Initial cut

--- libs/types/objectid.go
+++ libs/types/objectid.go
@@ -19,4 +19,69 @@ var (
 type GrapheneObject interface {
 	util.TypeMarshaler
 	util.TypeUnmarshaler
-	UnmarshalJ
+	UnmarshalJSON(s []byte) error
+	MarshalJSON() ([]byte, error)
+	ID() string
+	String() string
+	ObjectType() ObjectType
+	SpaceType() SpaceType
+	Instance() UInt64
+	Equals(id GrapheneObject) bool
+	Valid() bool
+}
+
+type GrapheneObjects []GrapheneObject
+
+func (p GrapheneObjects) ToStrings() []string {
+	ids := make([]string, len(p))
+	for idx, o := range p {
+		ids[idx] = o.ID()
+	}
+
+	return ids
+}
+
+func (p GrapheneObjects) String() string {
+	return strings.Join(p.ToStrings(), " ")
+}
+
+type ObjectIDs []ObjectID
+
+func (p ObjectIDs) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.EncodeUVarint(uint64(len(p))); err != nil {
+		return errors.Annotate(err, "encode length")
+	}
+
+	for _, ex := range p {
+		if err := enc.Encode(ex); err != nil {
+			return errors.Annotate(err, "encode ObjectID")
+		}
+	}
+
+	return nil
+}
+
+type ObjectID struct {
+	number UInt64
+}
+
+func (p ObjectID) Marshal(enc *util.TypeEncoder) error {
+	if err := enc.EncodeUVarint(uint64(p.number)); err != nil {
+		return errors.Annotate(err, "encode number")
+	}
+
+	return nil
+}
+
+func (p *ObjectID) Unmarshal(dec *util.TypeDecoder) error {
+	var ins uint64
+	if err := dec.DecodeUVarint(&ins); err != nil {
+		return errors.Annotate(err, "decode instance")
+	}
+
+	p.number = UInt64(ins)
+	return nil
+}
+
+func (p ObjectID) MarshalJSON() ([]byte, error) {
+	return ffjson.Marsha
