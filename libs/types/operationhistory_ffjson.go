da4f8e408af80934ed4ae2a1980fe2fe55ea1919
Fidecity <101517756+Fidecity@users.noreply.github.com>
2022-06-07 17:37:11 +0000

    wow?

--- libs/types/operationhistory_ffjson.go
+++ libs/types/operationhistory_ffjson.go
@@ -248,4 +248,51 @@ mainparse:
 				}
 
 				if fflib.EqualFoldRight(ffjKeyOperationHistoryTrxInBlock, kn) {
-					curren
+					currentKey = ffjtOperationHistoryTrxInBlock
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.EqualFoldRight(ffjKeyOperationHistoryBlockNum, kn) {
+					currentKey = ffjtOperationHistoryBlockNum
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				if fflib.SimpleLetterEqualFold(ffjKeyOperationHistoryID, kn) {
+					currentKey = ffjtOperationHistoryID
+					state = fflib.FFParse_want_colon
+					goto mainparse
+				}
+
+				currentKey = ffjtOperationHistorynosuchkey
+				state = fflib.FFParse_want_colon
+				goto mainparse
+			}
+
+		case fflib.FFParse_want_colon:
+			if tok != fflib.FFTok_colon {
+				wantedTok = fflib.FFTok_colon
+				goto wrongtokenerror
+			}
+			state = fflib.FFParse_want_value
+			continue
+		case fflib.FFParse_want_value:
+
+			if tok == fflib.FFTok_left_brace || tok == fflib.FFTok_left_bracket || tok == fflib.FFTok_integer || tok == fflib.FFTok_double || tok == fflib.FFTok_string || tok == fflib.FFTok_bool || tok == fflib.FFTok_null {
+				switch currentKey {
+
+				case ffjtOperationHistoryID:
+					goto handle_ID
+
+				case ffjtOperationHistoryBlockNum:
+					goto handle_BlockNum
+
+				case ffjtOperationHistoryTrxInBlock:
+					goto handle_TrxInBlock
+
+				case ffjtOperationHistoryOpInTrx:
+					goto handle_OpInTrx
+
+				case ffjtOperationHistoryVirtualOp:
+					goto 
