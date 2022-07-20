5644607f685d71d8635b04971643a2859e0bc8f7
Fidecity <101517756+Fidecity@users.noreply.github.com>
2022-07-20 23:04:54 +0000

    update backlog

--- libs/types/operationhistory_ffjson.go
+++ libs/types/operationhistory_ffjson.go
@@ -699,4 +699,71 @@ mainparse:
 			}
 			state = fflib.FFParse_want_value
 			continue
-		case fflib.FFPar
+		case fflib.FFParse_want_value:
+
+			if tok == fflib.FFTok_left_brace || tok == fflib.FFTok_left_bracket || tok == fflib.FFTok_integer || tok == fflib.FFTok_double || tok == fflib.FFTok_string || tok == fflib.FFTok_bool || tok == fflib.FFTok_null {
+				switch currentKey {
+
+				case ffjtOperationRelativeHistoryMemo:
+					goto handle_Memo
+
+				case ffjtOperationRelativeHistoryDescription:
+					goto handle_Description
+
+				case ffjtOperationRelativeHistoryOp:
+					goto handle_Op
+
+				case ffjtOperationRelativeHistorynosuchkey:
+					err = fs.SkipField(tok)
+					if err != nil {
+						return fs.WrapErr(err)
+					}
+					state = fflib.FFParse_after_value
+					goto mainparse
+				}
+			} else {
+				goto wantedvalue
+			}
+		}
+	}
+
+handle_Memo:
+
+	/* handler: j.Memo type=types.Buffer kind=slice quoted=false*/
+
+	{
+		if tok == fflib.FFTok_null {
+
+		} else {
+
+			tbuf, err := fs.CaptureField(tok)
+			if err != nil {
+				return fs.WrapErr(err)
+			}
+
+			err = j.Memo.UnmarshalJSON(tbuf)
+			if err != nil {
+				return fs.WrapErr(err)
+			}
+		}
+		state = fflib.FFParse_after_value
+	}
+
+	state = fflib.FFParse_after_value
+	goto mainparse
+
+handle_Description:
+
+	/* handler: j.Description type=types.String kind=struct quoted=false*/
+
+	{
+		if tok == fflib.FFTok_null {
+
+		} else {
+
+			tbuf, err := fs.CaptureField(tok)
+			if err != nil {
+				return fs.WrapErr(err)
+			}
+
+		
