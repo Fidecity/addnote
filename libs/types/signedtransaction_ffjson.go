5bff8fa0724b909a30b882f662eca69c0b453cd3
Fidecity <101517756+Fidecity@users.noreply.github.com>
2022-11-10 10:57:37 +0000

    saa7134: Fix unlocked snd_pcm_stop() call

--- libs/types/signedtransaction_ffjson.go
+++ libs/types/signedtransaction_ffjson.go
@@ -160,4 +160,58 @@ mainparse:
 		case fflib.FFParse_after_value:
 			if tok == fflib.FFTok_comma {
 				state = fflib.FFParse_want_key
-			} e
+			} else if tok == fflib.FFTok_right_bracket {
+				goto done
+			} else {
+				wantedTok = fflib.FFTok_comma
+				goto wrongtokenerror
+			}
+
+		case fflib.FFParse_want_key:
+			// json {} ended. goto exit. woo.
+			if tok == fflib.FFTok_right_bracket {
+				goto done
+			}
+			if tok != fflib.FFTok_string {
+				wantedTok = fflib.FFTok_string
+				goto wrongtokenerror
+			}
+
+			kn := fs.Output.Bytes()
+			if len(kn) <= 0 {
+				// "" case. hrm.
+				currentKey = ffjtSignedTransactionnosuchkey
+				state = fflib.FFParse_want_colon
+				goto mainparse
+			} else {
+				switch kn[0] {
+
+				case 'e':
+
+					if bytes.Equal(ffjKeySignedTransactionExpiration, kn) {
+						currentKey = ffjtSignedTransactionExpiration
+						state = fflib.FFParse_want_colon
+						goto mainparse
+
+					} else if bytes.Equal(ffjKeySignedTransactionExtensions, kn) {
+						currentKey = ffjtSignedTransactionExtensions
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				case 'o':
+
+					if bytes.Equal(ffjKeySignedTransactionOperations, kn) {
+						currentKey = ffjtSignedTransactionOperations
+						state = fflib.FFParse_want_colon
+						goto mainparse
+					}
+
+				case 'r':
+
+					if bytes.Equal(ffjKeySignedTransactionRefBlockNum, kn) {
+						currentKey = ffjtSignedTransactionRefBlockNum
+						state = fflib.FFParse_want_colon
+						goto mainparse
+
+					} else if byt
