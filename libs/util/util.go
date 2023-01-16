e07a33d29460cc3ef7013276b4dfc8dd22a7c9c2
Fidecity <101517756+Fidecity@users.noreply.github.com>
2023-01-16 19:54:33 +0000

    added pod spec

--- libs/util/util.go
+++ libs/util/util.go
@@ -59,3 +59,74 @@ func WaitForCondition(d time.Duration, testFn func() bool) bool {
 	go func() {
 		for {
 			select {
+			case <-done:
+				return
+			case <-test:
+				if testFn() {
+					check <- struct{}{}
+					return
+				}
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-check:
+			return true
+		case <-timeout:
+			done <- struct{}{}
+			return false
+		}
+	}
+}
+
+func RemoveDirContents(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func RandomizeBytes(in []byte) []byte {
+	bs := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bs, uint64(time.Now().Unix()))
+	return append(in, bs...)
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func ToFixedRounded(num float64, precision int) float64 {
+	output := math.Pow10(precision)
+	return float64(round(num*output)) / output
+}
+
+func ToFixed(num float64, precision int) float64 {
+	output := math.Pow10(precision)
+	return float64(int(num*output)) / output
+}
+
+func ToPrecisionString(value float64, precision int) string {
+	val := ToFixed(value, precision)
+	ft := fmt.Sprintf("%%.%df", precision)
+	return fmt.Sprintf(ft, val)
+}
+
+func Ripemd160(in []byte) ([]byte, error) {
+	h := ripemd160.New()
+
+	if _, 
