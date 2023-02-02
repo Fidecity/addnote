72b7d10e84f8d833a6dedf3f9558c95978fb54e8
Fidecity <101517756+Fidecity@users.noreply.github.com>
2023-02-02 13:32:35 +0000

    update schema

--- openwtester/manager_test.go
+++ openwtester/manager_test.go
@@ -34,4 +34,66 @@ func TestWalletManager_CreateWallet(t *testing.T) {
 	w := &openwallet.Wallet{Alias: "HELLO XWC", IsTrust: true, Password: "12345678"}
 	nw, key, err := tm.CreateWallet(testApp, w)
 	if err != nil {
-		log.Error(
+		log.Error(err)
+		return
+	}
+
+	log.Info("wallet:", nw)
+	log.Info("key:", key)
+
+}
+
+func TestWalletManager_GetWalletInfo(t *testing.T) {
+
+	tm := testInitWalletManager()
+
+	wallet, err := tm.GetWalletInfo(testApp, "WCYrRzsTTEW5NcGMiPabgxbkyJ2PsQTkZm")
+	if err != nil {
+		log.Error("unexpected error:", err)
+		return
+	}
+	log.Info("wallet:", wallet)
+}
+
+func TestWalletManager_GetWalletList(t *testing.T) {
+
+	tm := testInitWalletManager()
+
+	list, err := tm.GetWalletList(testApp, 0, 10000000)
+	if err != nil {
+		log.Error("unexpected error:", err)
+		return
+	}
+	for i, w := range list {
+		log.Info("wallet[", i, "] :", w)
+	}
+	log.Info("wallet count:", len(list))
+
+	tm.CloseDB(testApp)
+}
+
+func TestWalletManager_CreateAssetsAccount(t *testing.T) {
+
+	tm := testInitWalletManager()
+
+	walletID := "WCYrRzsTTEW5NcGMiPabgxbkyJ2PsQTkZm"
+	account := &openwallet.AssetsAccount{Alias: "mainnetXWC", WalletID: walletID, Required: 1, Symbol: "XWC", IsTrust: true}
+	account, address, err := tm.CreateAssetsAccount(testApp, walletID, "12345678", account, nil)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	log.Info("account:", account)
+	log.Info("address:", address)
+
+	tm.CloseDB(testApp)
+}
+
+func TestWalletManager_GetAssetsAccountList(t *testing.T) {
+
+	tm := testInitWalletManager()
+
+	walletID := "WCYrRzsTTEW5NcGMiPabgxbkyJ2PsQTkZm"
+	list, err := tm.GetAssetsAccountList(testApp, walletID, 0, 10000000)
+	if e
