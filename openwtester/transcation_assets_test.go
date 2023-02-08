8b3c4d816c831365ad5bb951d34bc6f91b7085a3
Fidecity <101517756+Fidecity@users.noreply.github.com>
2023-02-08 09:13:47 +0000

    Fix deploy script

--- openwtester/transcation_assets_test.go
+++ openwtester/transcation_assets_test.go
@@ -3,4 +3,42 @@
  * This file is part of the openwallet library.
  *
  * The openwallet library is free software: you can redistribute it and/or modify
- * it under the term
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The openwallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package openwtester
+
+import (
+	"github.com/blocktree/openwallet/v2/openw"
+	"testing"
+
+	"github.com/blocktree/openwallet/v2/log"
+	"github.com/blocktree/openwallet/v2/openwallet"
+)
+
+func testGetAssetsAccountBalance(tm *openw.WalletManager, walletID, accountID string) {
+	balance, err := tm.GetAssetsAccountBalance(testApp, walletID, accountID)
+	if err != nil {
+		log.Error("GetAssetsAccountBalance failed, unexpected error:", err)
+		return
+	}
+	log.Info("balance:", balance)
+}
+
+func testGetAssetsAccountTokenBalance(tm *openw.WalletManager, walletID, accountID string, contract openwallet.SmartContract) {
+	balance, err := tm.GetAssetsAccountTokenBalance(testApp, walletID, accountID, contract)
+	if err != nil {
+		log.Error("GetAssetsAccountTokenBalance failed, unexpected error:", err)
+		return
+	}
+	log.Info("token balance:", balance.Balance)
+}
+
+func testCreateTransactionStep(tm *openw.WalletManager, walletID, accountID, to, amoun
