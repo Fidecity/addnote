1d1c11b71ad6ad02a09722753758f6c086b49511
Fidecity <101517756+Fidecity@users.noreply.github.com>
2023-03-05 20:11:18 +0000

    Update WaderTGbot.lua

--- whitecoin/contract.go
+++ whitecoin/contract.go
@@ -2,4 +2,46 @@
  * Copyright 2018 The OpenWallet Authors
  * This file is part of the OpenWallet library.
  *
- * The OpenWallet library is free software: you can redistribu
+ * The OpenWallet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The OpenWallet library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ */
+
+package whitecoin
+
+import (
+	"github.com/blocktree/openwallet/v2/openwallet"
+	"github.com/Assetsadapter/whitecoin-adapter/types"
+	"github.com/shopspring/decimal"
+)
+
+type ContractDecoder struct {
+	openwallet.SmartContractDecoderBase
+	wm *WalletManager
+}
+
+//NewContractDecoder 智能合约解析器
+func NewContractDecoder(wm *WalletManager) *ContractDecoder {
+	decoder := ContractDecoder{}
+	decoder.wm = wm
+	return &decoder
+}
+
+// GetTokenBalanceByAddress return the balance by account alias, queried by rpc
+func (decoder *ContractDecoder) GetTokenBalanceByAddress(contract openwallet.SmartContract, address ...string) ([]*openwallet.TokenBalance, error) {
+
+	var (
+		asset = types.MustParseObjectID(contract.Address)
+	)
+	tokenBalanceList := make([]*openwallet.TokenBalance, 0)
+
+	for _, addr := range address {
+
+		balance, err := decoder.wm.Api.GetAddrBalance(addr, asset)
+		if err 
