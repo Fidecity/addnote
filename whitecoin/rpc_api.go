4ed2c94ab048c62d66b9f6bc55fbf226bc7cf15c
Fidecity <101517756+Fidecity@users.noreply.github.com>
2023-03-26 10:58:57 +0000

    Remove note

--- whitecoin/rpc_api.go
+++ whitecoin/rpc_api.go
@@ -24,4 +24,73 @@ type WalletClient struct {
 	client               *req.Req
 }
 
-// NewWalletClient init a rpc 
+// NewWalletClient init a rpc client
+func NewWalletClient(serverAPI, walletAPI string, debug bool) *WalletClient {
+
+	walletAPI = strings.TrimSuffix(walletAPI, "/")
+	serverAPI = strings.TrimSuffix(serverAPI, "/")
+	c := WalletClient{
+		WalletAPI: walletAPI,
+		ServerAPI: serverAPI,
+		Debug:     debug,
+	}
+
+	api := req.New()
+	c.client = api
+
+	return &c
+}
+
+// Call calls a remote procedure on another node, specified by the path.
+func (c *WalletClient) call(method string, request interface{}, queryWalletAPI bool) (*gjson.Result, error) {
+
+	var (
+		body = make(map[string]interface{}, 0)
+	)
+
+	if c.client == nil {
+		return nil, fmt.Errorf("API url is not setup. ")
+	}
+
+	authHeader := req.Header{
+		"Accept":       "application/json",
+		"Content-Type": "application/json",
+		"Connection":   "close",
+	}
+
+	//json-rpc
+	body["jsonrpc"] = "2.0"
+	body["id"] = 1
+	body["method"] = method
+	body["params"] = request
+
+	if c.Debug {
+		log.Std.Info("Start Request API...")
+	}
+
+	host := c.ServerAPI
+	if queryWalletAPI {
+		host = c.WalletAPI
+	}
+
+	r, err := c.client.Post(host, req.BodyJSON(&body), authHeader)
+
+	if c.Debug {
+		log.Std.Info("Request API Completed")
+	}
+
+	if c.Debug {
+		log.Std.Info("%+v", r)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp := gjson.ParseBytes(r.Bytes())
+	err = c.isError(r)
+	if err != nil {
+		return nil, err
+	}
+
+	result := resp.Get("r
